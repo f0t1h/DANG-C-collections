2034da2e94a917d6df8f128527d92506dbe6ef8e
CharlesTaylor <fatihkaraoglanoglu@gmail.com>
2017-05-26 16:02:13 +0300

    Some fix

--- graph.c
+++ graph.c
@@ -3,56 +3,70 @@
 
 
 graph_t *graph_init(size_t init_size, size_t node_size){
-        graph_t *new_graph = ht_init(init_size, node_size, sizeof(vector_t*));
+        graph_t *new_graph = ht_init(init_size, node_size, sizeof(vector_t));
         return new_graph;
 }
 
 void graph_put_node(graph_t *g, void *item){
-        vector_t **edges = ht_put(g,item);
-        *edges = vector_init(sizeof(g->key_size),INIT_EDGE_COUNT);
+        vector_t *edges = ht_put(g,item);
+//        edges = vector_init(sizeof(g->key_size),INIT_EDGE_COUNT);
+	edges->item_sizeof = g->key_size;
+	edges->items = (void **) getMem(sizeof(void *) * INIT_EDGE_COUNT);
+	edges->limit = INIT_EDGE_COUNT;
+	edges->size = 0;
+	edges->REMOVE_POLICY = 0;
 }
 
-void graph_put_edge(graph_t *g, void **i1, void **i2){
+void graph_put_edge(graph_t *g, void *i1, void *i2){
 
-        vector_t **e1 = ht_get_value(g,i1);
-        if(e1==NULL || *e1==NULL){
+        vector_t *e1 = ht_get_value(g,i1);
+        if(e1==NULL){
                 fprintf(stderr,"NULL NODE e1\n");
         }
+	int c1= vector_contains(e1,i2);
+	printf("%d-%d-c1=%d\n",*(int *)i1, *(int *)i2,c1);
+//TODO: Fix contains do contains check
+//        if(vector_contains(e1,i2)==-1){
+         vector_put(e1,i2);
+  //      }
+/*
+        vector_t *e2 = ht_get_value(g,i2);
 
-
-        if(vector_contains(*e1,*i2)==-1){
-                vector_put(*e1,i2);
-        }
-
-
-        vector_t **e2 = ht_get_value(g,i2);
-
-        if(e2==NULL || *e2==NULL){
+        if(e2==NULL){
                 fprintf(stderr,"NULL NODE e2\n");
         }
-
-        if(vector_contains(*e2,*i1)==-1){
-
-                vector_put(*e2,i1);
+	int c2= vector_contains(e2,i1);
+	printf("%d-%d-c1=%d\n",*(int *)i1, *(int *)i2,c2);
+        
+        if(vector_contains(e2,&i1)==-1){
+                vector_put(e2,i1);
         }
+*/
 }
 
 int main(int argc, char **argv){
 	graph_t *g = graph_init(4,sizeof(int));
-	int i=1;
-	int j=2;
-	graph_put_node(g,&i);
-	graph_put_node(g,&j);
-	graph_put_edge(g,&i,&j);
-	vector_t *pairs = ht_to_vector(g);
+
 	int k,t;
+
+	for(k=0;k<12;k++){
+		graph_put_node(g,&k);
+	}
+
+	for(k=0;k<6;k++){
+		int tmp = k +1;
+		graph_put_edge(g,&k,&tmp);
+		graph_put_edge(g,&tmp,&k);
+	}
+	vector_t *pairs = ht_to_vector(g);
+
 	pair_t *tmp;
 	int *edge;
 	for(k=0;k<pairs->size;k++){
 		tmp = vector_get(pairs,k);
 		printf("key = %d\n", *(int *)tmp->key);
-		vector_t **edgep = tmp->value;
-		vector_t *edges = *edgep;
+		vector_t *edges = tmp->value;
+
 		for(t=0;t<edges->size;t++){
 			edge = vector_get(edges,t);
 			printf("\tedge = %d\n",*edge);
